c4f7629cb46bd931c79c71bf5ce95c590db9fce9
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-04-10 10:56:12 -0500

    Improve project security.

--- customersurvey/customersurvey/settings.py
+++ customersurvey/customersurvey/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ur^nh81=omzwlaodhv5&se1@&1u2223+2jx83rpy!973xtjm-y'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
